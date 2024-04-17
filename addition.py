294bc92e37d2dd9371d4d1a20294cbff5cd70e46
Eleanor E <eleanorezimah@gmail.com>
2024-04-16 23:12:27 -0400

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,23 @@
+# def addition(a, b):
+#     return a+b
+
+# # Main program
+# num1 = float(input('Enter your 1st number\n'))
+# num2 = float(input('Enter your 2nd number:\n'))
+
+# # Calling our function
+# result = addition(num1, num2)
+# print('The result is', result)
+
+def addition(a, b):
+    return a+b
+
+def main():
+    num1 = float(input('Enter your 1st number\n'))
+    num2 = float(input('Enter your 2nd number:\n'))
+
+    # Calling our function
+    result = addition(num1, num2)
+    print('The result is', result)
+
+main()
